d8d07d5834ace579649a64faabd0c9bce3041346
Daniel Corn <cod@iresults.li>
2020-09-25 16:22:23 +0200

    Add trait for Twig templates

--- Block/TwigTemplate.php
+++ Block/TwigTemplate.php
@@ -4,54 +4,19 @@ declare(strict_types=1);
 namespace Iresults\M2Twig\Block;
 
 use Iresults\M2Twig\Framework\View\TemplateEngine\Twig as TwigTemplateEngine;
-use Magento\Framework\Profiler;
+use Iresults\M2Twig\TwigTemplateTrait;
 use Magento\Framework\View\Element\Template;
-use Twig\TwigFilter;
-use Twig\TwigFunction;
-use function pathinfo;
-use function strpos;
-use const PATHINFO_EXTENSION;
 
 class TwigTemplate extends Template
 {
+    use TwigTemplateTrait;
+
     public function fetchView($fileName)
     {
         if (!$this->validator->isValid($fileName)) {
             return parent::fetchView($fileName);
-        }
-
-        $relativeFilePath = $this->getRootDirectory()->getRelativePath($fileName);
-        Profiler::start(
-            'TEMPLATE:' . $fileName,
-            ['group' => 'TEMPLATE', 'file_name' => $relativeFilePath]
-        );
-
-        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
-        /** @var TwigTemplateEngine $templateEngine */
-        $templateEngine = clone $this->templateEnginePool->get($extension);
-
-        $this->addDefaultFunctionsAndFilters($templateEngine);
-        $this->prepareEngine($templateEngine);
-
-        $templateVariables = array_merge(
-            $this->getAdditionalViewVars(),
-            $this->getData(),
-            $this->_viewVars,
-            ['block' => $this]
-        );
-
-        $html = $templateEngine->render($this->templateContext, $fileName, $templateVariables);
-        Profiler::stop('TEMPLATE:' . $fileName);
-
-        return $html;
-    }
-
-    public function getAssetUrl($asset, array $params = [])
-    {
-        if (strpos($asset, '::')) {
-            return $this->getViewFileUrl($asset, $params);
         } else {
-            return $this->getViewFileUrl($this->getModuleName() . '::' . $asset, $params);
+            return $this->fetchTwigView($fileName, $this->templateEnginePool, $this->templateContext, $this->getData());
         }
     }
 
@@ -74,23 +39,5 @@ class TwigTemplate extends Template
     protected function getAdditionalViewVars(): array
     {
         return [];
-    }
-
-    protected function registerTwigFunction(TwigTemplateEngine $templateEngine, string $name, callable $callback)
-    {
-        $templateEngine->addFunction(new TwigFunction($name, $callback));
-    }
-
-    protected function registerTwigFilter(TwigTemplateEngine $templateEngine, string $name, callable $callback)
-    {
-        $templateEngine->addFilter(new TwigFilter($name, $callback));
-    }
-
-    private function addDefaultFunctionsAndFilters(TwigTemplateEngine $templateEngine)
-    {
-        //$templateEngine->addFilter(new TwigFilter('url', [$this, 'getUrl']));
-        //$templateEngine->addFunction(new TwigFunction('url', [$this, 'getUrl']));
-        $this->registerTwigFunction($templateEngine, 'viewFileUrl', [$this, 'getViewFileUrl']);
-        $this->registerTwigFunction($templateEngine, 'assetUrl', [$this, 'getAssetUrl']);
     }
 }
---
+++ TwigTemplateTrait.php
@@ -0,0 +1,138 @@
+<?php
+declare(strict_types=1);
+
+namespace Iresults\M2Twig;
+
+use Iresults\M2Twig\Framework\View\TemplateEngine\Twig as TwigTemplateEngine;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Profiler;
+use Magento\Framework\View\Element\AbstractBlock;
+use Magento\Framework\View\Element\BlockInterface;
+use Magento\Framework\View\TemplateEnginePool;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use function array_merge;
+use function get_class;
+use function method_exists;
+use function pathinfo;
+use function strpos;
+use const PATHINFO_EXTENSION;
+
+trait TwigTemplateTrait
+{
+    /**
+     * Retrieve url of a view file
+     *
+     * @param string $fileId
+     * @param array  $params
+     * @return string
+     */
+    abstract public function getViewFileUrl($fileId, array $params = []);
+
+    /**
+     * Template context
+     *
+     * @var BlockInterface
+     */
+    protected $templateContext;
+
+    public function fetchTwigView(
+        string $fileName,
+        TemplateEnginePool $templateEnginePool,
+        BlockInterface $templateContext,
+        array $data
+    ) {
+        $relativeFilePath = $this->detectRelativeFilePath($fileName);
+        Profiler::start(
+            'TEMPLATE:' . $fileName,
+            ['group' => 'TEMPLATE', 'file_name' => $relativeFilePath]
+        );
+
+        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+        /** @var TwigTemplateEngine $templateEngine */
+        $templateEngine = clone $templateEnginePool->get($extension);
+
+        $this->addDefaultFunctionsAndFilters($templateEngine);
+        $this->prepareEngine($templateEngine);
+
+        $templateVariables = array_merge(
+            $this->getAdditionalViewVars(),
+            $data,
+            $this->_viewVars ?? [],
+            ['block' => $this]
+        );
+
+        $html = $templateEngine->render($templateContext, $fileName, $templateVariables);
+        Profiler::stop('TEMPLATE:' . $fileName);
+
+        return $html;
+    }
+
+    public function getAssetUrl($asset, array $params = [])
+    {
+        if (strpos($asset, '::')) {
+            return $this->getViewFileUrl($asset, $params);
+        } elseif (method_exists($this, 'getModuleName')) {
+            return $this->getViewFileUrl($this->getModuleName() . '::' . $asset, $params);
+        } else {
+            return $this->getViewFileUrl(AbstractBlock::extractModuleName(get_class($this)) . '::' . $asset, $params);
+        }
+    }
+
+    /**
+     * Prepare the Twig Engine
+     *
+     * Overwrite this method to add additional Filters or Functions to Twig
+     *
+     * @param TwigTemplateEngine $templateEngine
+     */
+    protected function prepareEngine(TwigTemplateEngine $templateEngine)
+    {
+    }
+
+    /**
+     * Return a dictionary of additional view variables
+     *
+     * @return array
+     */
+    protected function getAdditionalViewVars(): array
+    {
+        return [];
+    }
+
+    protected function registerTwigFunction(TwigTemplateEngine $templateEngine, string $name, callable $callback)
+    {
+        $templateEngine->addFunction(new TwigFunction($name, $callback));
+    }
+
+    protected function registerTwigFilter(TwigTemplateEngine $templateEngine, string $name, callable $callback)
+    {
+        $templateEngine->addFilter(new TwigFilter($name, $callback));
+    }
+
+    private function addDefaultFunctionsAndFilters(TwigTemplateEngine $templateEngine)
+    {
+        //$templateEngine->addFilter(new TwigFilter('url', [$this, 'getUrl']));
+        //$templateEngine->addFunction(new TwigFunction('url', [$this, 'getUrl']));
+        $this->registerTwigFunction($templateEngine, 'viewFileUrl', [$this, 'getViewFileUrl']);
+        $this->registerTwigFunction($templateEngine, 'assetUrl', [$this, 'getAssetUrl']);
+    }
+
+    /**
+     * @param string $fileName
+     * @return string
+     */
+    private function detectRelativeFilePath(string $fileName)
+    {
+        if (method_exists($this, 'getRootDirectory')) {
+            return $this->getRootDirectory()->getRelativePath($fileName);
+        } else {
+            $objectManager = ObjectManager::getInstance();
+            $filesystem = $objectManager->create(Filesystem::class);
+
+            return $filesystem->getDirectoryRead(DirectoryList::ROOT)->getRelativePath($fileName);
+        }
+    }
+}
