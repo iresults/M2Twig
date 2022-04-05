15b2af240c62d52a63be57ff8ec9035e81f78ede
Daniel Corn <daniel@iresults.li>
2022-04-05 12:03:38 +0200

    Add factory for Twig Environment

--- src/Framework/View/TemplateEngine/Twig.php
+++ src/Framework/View/TemplateEngine/Twig.php
@@ -1,27 +1,24 @@
-<?php /** @noinspection PhpFullyQualifiedNameUsageInspection */
-/** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
+<?php
 declare(strict_types=1);
 
 namespace Iresults\M2Twig\Framework\View\TemplateEngine;
 
+use Iresults\M2Twig\TwigEnvironmentFactory;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
-use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\BlockInterface;
+use Magento\Framework\View\TemplateEngine\Php;
 use Twig\Environment;
 use Twig\Error\Error;
-use Twig\Extension\DebugExtension;
 use Twig\TemplateWrapper;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
-class Twig extends \Magento\Framework\View\TemplateEngine\Php
+class Twig extends Php
 {
-    private const TWIG_CACHE_DIR = 'twig';
-
     /**
      * @var Environment
      */
@@ -38,13 +35,6 @@ class Twig extends \Magento\Framework\View\TemplateEngine\Php
     private $directoryList;
 
     /**
-     * Event manager
-     *
-     * @var ManagerInterface
-     */
-    private $eventManager;
-
-    /**
      * Url Builder
      *
      * @var UrlInterface
@@ -55,26 +45,22 @@ class Twig extends \Magento\Framework\View\TemplateEngine\Php
      * @param ObjectManagerInterface $helperFactory
      * @param DirectoryList          $directoryList
      * @param ScopeConfigInterface   $scopeConfig
-     * @param ManagerInterface       $eventManager
      * @param UrlInterface           $urlBuilder
-     * @param Environment            $twig
+     * @param TwigEnvironmentFactory $environmentFactory
      * @throws FileSystemException
      */
     public function __construct(
         ObjectManagerInterface $helperFactory,
         DirectoryList $directoryList,
         ScopeConfigInterface $scopeConfig,
-        ManagerInterface $eventManager,
         UrlInterface $urlBuilder,
-        Environment $twig
+        TwigEnvironmentFactory $environmentFactory
     ) {
         parent::__construct($helperFactory);
         $this->directoryList = $directoryList;
         $this->scopeConfig = $scopeConfig;
-        $this->eventManager = $eventManager;
-        $this->twig = $twig;
+        $this->twig = $environmentFactory->create();
         $this->urlBuilder = $urlBuilder;
-        $this->initTwig();
     }
 
     public function render(BlockInterface $block, $fileName, array $dictionary = [])
@@ -96,37 +82,6 @@ class Twig extends \Magento\Framework\View\TemplateEngine\Php
         return $result;
     }
 
-    /**
-     * Initialises Twig with all Magento 2 necessary functions
-     *
-     * @throws FileSystemException
-     */
-    private function initTwig()
-    {
-        $this->twig->setCache($this->getCachePath());
-        if ($this->isDebugEnabled()) {
-            $this->twig->enableDebug();
-        } else {
-            $this->twig->disableDebug();
-        }
-        if ($this->scopeConfig->isSetFlag('dev/twig/auto_reload') || $this->isDebugEnabled()) {
-            $this->twig->enableAutoReload();
-        } else {
-            $this->twig->disableAutoReload();
-        }
-        if ($this->scopeConfig->isSetFlag('dev/twig/strict_variables')) {
-            $this->twig->enableStrictVariables();
-        } else {
-            $this->twig->disableStrictVariables();
-        }
-        $this->twig->setCharset($this->scopeConfig->getValue('dev/twig/charset'));
-
-        $this->registerDefaultFiltersAndFunctions();
-        $this->twig->addExtension(new DebugExtension());
-
-        $this->eventManager->dispatch('twig_init', ['twig' => $this->twig]);
-    }
-
     public function addFunction(TwigFunction $function): self
     {
         $this->twig->addFunction($function);
@@ -141,42 +96,15 @@ class Twig extends \Magento\Framework\View\TemplateEngine\Php
         return $this;
     }
 
-    /**
-     * @return string
-     * @throws FileSystemException
-     */
-    private function getCachePath()
-    {
-        if (false === $this->scopeConfig->isSetFlag('dev/twig/cache')) {
-            return false;
-        }
-
-        return $this->directoryList->getPath(DirectoryList::VAR_DIR) . DIRECTORY_SEPARATOR . self::TWIG_CACHE_DIR;
-    }
-
     private function getTemplate(string $fileName): TemplateWrapper
     {
         $path = str_replace($this->directoryList->getPath(DirectoryList::ROOT) . DIRECTORY_SEPARATOR, '', $fileName);
 
         return $this->twig->load($path);
-        //return $this->twig->loadTemplate($path);
     }
 
-    /**
-     * @return bool
-     */
     private function isDebugEnabled(): bool
     {
         return $this->scopeConfig->isSetFlag('dev/twig/debug');
-    }
-
-    private function registerDefaultFiltersAndFunctions(): void
-    {
-        $this->twig->addFunction(new TwigFunction('helper', [$this, 'helper']));
-        $this->twig->addFilter(new TwigFilter('translate', '__'));
-        $this->twig->addFunction(new TwigFunction('translate', '__'));
-
-        $this->twig->addFilter(new TwigFilter('url', [$this->urlBuilder, 'getUrl']));
-        $this->twig->addFunction(new TwigFunction('url', [$this->urlBuilder, 'getUrl']));
     }
 }
---
+++ src/TwigEnvironmentFactory.php
@@ -0,0 +1,142 @@
+<?php
+declare(strict_types=1);
+
+namespace Iresults\M2Twig;
+
+use Iresults\M2Twig\Twig\Loader\FilesystemLoader;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\UrlInterface;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\TemplateWrapper;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use function str_replace;
+use const DIRECTORY_SEPARATOR;
+
+class TwigEnvironmentFactory
+{
+    private const TWIG_CACHE_DIR = 'twig';
+
+    /**
+     * @var Environment
+     */
+    private $environment;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var DirectoryList
+     */
+    private $directoryList;
+
+    /**
+     * Event manager
+     *
+     * @var ManagerInterface
+     */
+    private $eventManager;
+
+    /**
+     * Url Builder
+     *
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+
+    /**
+     * @var FilesystemLoader
+     */
+    private $filesystemLoader;
+
+    public function __construct(
+        DirectoryList $directoryList,
+        ScopeConfigInterface $scopeConfig,
+        ManagerInterface $eventManager,
+        UrlInterface $urlBuilder,
+        Environment $environment,
+        FilesystemLoader $filesystemLoader
+    ) {
+        $this->directoryList = $directoryList;
+        $this->scopeConfig = $scopeConfig;
+        $this->eventManager = $eventManager;
+        $this->environment = $environment;
+        $this->urlBuilder = $urlBuilder;
+        $this->filesystemLoader = $filesystemLoader;
+    }
+
+    /**
+     * Initialises Twig with all Magento 2 necessary functions
+     *
+     * @throws FileSystemException
+     */
+    public function create(): Environment
+    {
+        $environment = clone $this->environment;
+        $environment->setCache($this->getCachePath());
+        if ($this->isDebugEnabled()) {
+            $environment->enableDebug();
+        } else {
+            $environment->disableDebug();
+        }
+        if ($this->scopeConfig->isSetFlag('dev/twig/auto_reload') || $this->isDebugEnabled()) {
+            $environment->enableAutoReload();
+        } else {
+            $environment->disableAutoReload();
+        }
+        if ($this->scopeConfig->isSetFlag('dev/twig/strict_variables')) {
+            $environment->enableStrictVariables();
+        } else {
+            $environment->disableStrictVariables();
+        }
+        $environment->setLoader($this->filesystemLoader);
+        $environment->setCharset($this->scopeConfig->getValue('dev/twig/charset'));
+
+        $this->registerDefaultFiltersAndFunctions($environment);
+        $environment->addExtension(new DebugExtension());
+
+        $this->eventManager->dispatch('twig_init', ['twig' => $environment]);
+
+        return $environment;
+    }
+
+    /**
+     * @return string|false
+     * @throws FileSystemException
+     */
+    private function getCachePath()
+    {
+        if (false === $this->scopeConfig->isSetFlag('dev/twig/cache')) {
+            return false;
+        }
+
+        return $this->directoryList->getPath(DirectoryList::VAR_DIR) . DIRECTORY_SEPARATOR . self::TWIG_CACHE_DIR;
+    }
+
+    private function getTemplate(string $fileName): TemplateWrapper
+    {
+        $path = str_replace($this->directoryList->getPath(DirectoryList::ROOT) . DIRECTORY_SEPARATOR, '', $fileName);
+
+        return $this->environment->load($path);
+    }
+
+    private function isDebugEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag('dev/twig/debug');
+    }
+
+    private function registerDefaultFiltersAndFunctions(Environment $environment): void
+    {
+        $environment->addFilter(new TwigFilter('translate', '__'));
+        $environment->addFunction(new TwigFunction('translate', '__'));
+
+        $environment->addFilter(new TwigFilter('url', [$this->urlBuilder, 'getUrl']));
+        $environment->addFunction(new TwigFunction('url', [$this->urlBuilder, 'getUrl']));
+    }
+}
